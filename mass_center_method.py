aabea1e9401ef4c586abce352dab77f6ef409202
Leoni-inoeL <104641145+Leoni-inoeL@users.noreply.github.com>
2025-12-23 08:01:31 +0300

    Update mass_center_method.py
    Added compatibility with the HMI_Processor class

--- mass_center_method.py
+++ mass_center_method.py
@@ -1,20 +1,38 @@
 import numpy as np
 from uncertainty import calculate_edge_based_uncertainty
+from hmi_processor import HMI_Processor
+
+
+class MassCenterMethod(HMI_Processor):
+    def __init__(self):
+        super().__init__()
+        
+    def solar_center(self):
+        if self.data is None:
+            print("No data loaded. Call read_fits() first.")
+            return None, None, None
+        
+        return self.process_method(self.data)
+    
+    def process_method(self, data_clean):
+        abs_data = np.abs(data_clean)
+        threshold = np.percentile(abs_data, 80)
+        mask = abs_data > threshold
+
+        y_indices, x_indices = np.where(mask)
+        if len(x_indices) > 0:
+            center_x = np.mean(x_indices)
+            center_y = np.mean(y_indices)
+
+            center = (center_x, center_y)
+            uncertainty = calculate_edge_based_uncertainty(data_clean, center)
+
+            return (center_x, center_y), "Center of Mass", uncertainty
+
+        return None, "Center of Mass", None
 
 
 def center_of_mass(data_clean):
-    abs_data = np.abs(data_clean)
-    threshold = np.percentile(abs_data, 80)
-    mask = abs_data > threshold
-
-    y_indices, x_indices = np.where(mask)
-    if len(x_indices) > 0:
-        center_x = np.mean(x_indices)
-        center_y = np.mean(y_indices)
-
-        center = (center_x, center_y)
-        uncertainty = calculate_edge_based_uncertainty(data_clean, center)
-
-        return (center_x, center_y), "Center of Mass", uncertainty
-
-    return None, "Center of Mass", None
+    processor = MassCenterMethod()
+    processor.data = data_clean
+    return processor.solar_center()
